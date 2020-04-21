3e50ade3406ed149c8026ca20da4a0c2c2b21139
opgezwolle6 <opgezwolle6@yandex.ru>
2020-04-21 22:36:08 +0500

    first commit

---
+++ app/src/main/java/ru/startandroid/firstproject/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.startandroid.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
